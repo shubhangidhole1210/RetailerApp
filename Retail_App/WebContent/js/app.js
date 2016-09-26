a52c87aa2d0eae93b463cb6bd1ad16df50ef6fd0
shubhangi dhole <shubhangidhole12@gmail.com>
2016-09-26 19:25:21 +0530

    create index.html page,create login.html page,create app.js file, create signup.html

---
+++ Retail_App/WebContent/js/app.js
@@ -0,0 +1,204 @@
+var retailApp = angular.module('retailApp', [ 'ngRoute'])
+retailApp.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'login.html'
+	}).when('/home', {
+		templateUrl : "home.html"
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
+/*retailApp.directive('loading', function () {
+    return {
+      restrict: 'E',
+      replace:true,
+      template: '<div class="loading"><img src="../image/spinner.jpg" width="20" height="20" />LOADING...</div>',
+      link: function ($scope, $element, $attr) {
+            $scope.$watch('loading', function (val) {
+                if (val)
+                	{
+                    $(element).show();
+                	}
+                else
+                	{
+                    $(element).hide();
+                	}
+            });
+      }
+    }
+})
+*/
+retailApp.controller('loginCtrl', function($scope, $location,$http) {
+	$scope.login = function() {
+		var username = $scope.username;
+		var password = $scope.password;
+		$scope.errorMsg = '';
+		if ($scope.username == 'admin' && $scope.password == 'admin') {
+			$http.get("loginError.json").then(function(response)
+					{
+						
+						/*$scope.loading = false;*/
+						console.log((response.data.data.errorcode != '0000'));
+						if(response.data.data.errorcode!='0000'){
+							console.log("Error occurred!!!");
+							$scope.errorMsg = response.data.data.message;
+							
+						}else{
+							console.log("Login successful!!!");
+							$location.path('/home');
+							
+						}
+					});
+			
+
+		} else {
+			alert("invalid user name and password");
+		}
+		/* $scope.loading = true;*/
+		
+	};
+});
+
+retailApp.controller('signCtrl', function($scope) {
+    
+    $scope.submitForm = function() {
+
+        
+        if ($scope.userForm.$valid) {
+            alert('user registraion sucessfully');
+        }
+
+    };
+
+});
+
+
+
+/*var retailApp = angular.module('retailApp',['ngRoute']);
+
+retailApp.config([ '$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.when('/home', {
+            templateUrl : 'home.html',
+            
+        })
+        $routeProvider.when('/', {
+            templateUrl : 'login.html',
+            controller: 'loginCtrl'
+        }).otherwise({
+            redirectTo : 'index.html'
+        });
+        //$locationProvider.html5Mode(true); //Remove the '#' from URL.
+    }
+]);
+
+retailApp.controller("loginCtrl", function($scope, $location) {
+    $scope.submit = function() {
+        var username = $scope.username;
+        var password = $scope.password;
+        if (username == "admin" && password == "admin") {
+            $location.path("/home" );
+        } else {
+            alert('invalid username and password');
+        }
+    };
+});
+
+
+
+*/
+retailApp.controller('searchCtrl',function($scope,$http)
+		{
+			$http.get("products.json").then(function(response)
+			  {
+				 $scope.searchInfo = response.data;
+			  }		
+			);
+			
+			}		
+		);
+//		var app = angular.module ('cartApp',['angularPayment', 'mm.foundation',  'ngAnimate','angularSpinner'])
+		retailApp.controller('cartCtrl',function($scope, $http, $model)
+		{
+			$scope.cart=[];
+			$http.get('products.json').sucess(function(response)
+			{
+				$scope.products=response.products
+			});
+			$scope.addCart= function(product)
+			{
+				var found=false;
+				$scope.cart.forEach(function(item)
+				{
+					if(item.id==product.id)
+						{
+						item.quantity++;
+						found = true;
+						}
+				});
+				if(!found)
+					{
+					$scope.cart.push(angular.extend({quantity: 1}, product));
+					}
+			};
+			$scope.getCartPrice = function()
+			{
+				var total=0;
+				$scope.cart.forEach(function(product)
+				{
+					total += product.price * product.quantity;
+				});
+				return total;
+		};
+
+		$scope.checkout= function()
+		{
+			$model.open({
+				templateUrl: 'checkout.html',
+				controller: 'checkoutCtrl',
+				resolve:
+					{
+					totalAmount: $scope.getCartPrice
+					}
+			});
+			};
+		})
+		 retailApp.controller('CheckoutCtrl',function($scope,$totalAmount)
+		{
+			 $scope.totalAmount=totalAmount;
+			 $scope.onSubmit=function()
+			 {
+				 $scope.processing=true;
+			 };
+			$scope.stripeCallback = function(code,result)
+			{
+				$scpe.processing=false;
+				$scope.hideAlerts();
+		        if(result.error)
+		        	{
+		        	   $scope.stripeError= result.error.message;
+		        	}
+		        else
+		        	{
+		        	    $scope.stripeToken= result.id;
+		        	}
+			};
+			$scope.hideAlerts = function () {
+				$scope.stripeError = null;
+				$scope.stripeToken = null;
+			};
+			 
+			 });
+
+		/* retailApp.controller('signCtrl', function($scope) {
+           
+		        $scope.submitForm = function() {
+
+		            
+		            if ($scope.userForm.$valid) {
+		                alert('user registraion sucessfully');
+		            }
+
+		        };
+
+		    });*/
